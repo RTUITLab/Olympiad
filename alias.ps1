d67e6cb31cf7e239d448b3fd31ffc6794c3cdfdb
Maksim Makushchenko <maksalmak@gmail.com>
2020-10-09 18:39:50 +0300

    Create prod file for overlay network

--- alias.ps1
+++ alias.ps1
@@ -2,7 +2,7 @@ function OlympiadDockerCompose {
     docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml -f .\Olympiad-Back\docker-compose.yml -f .\Olympiad-Back\docker-compose.override.yml -f .\Olympiad-Front\docker-compose.yml -f .\Olympiad-Front\docker-compose.override.yml $args
 }
 function OlympiadDockerComposeStack {
-    docker-compose -f .\docker-compose.yml -f .\Olympiad-Back\docker-compose.yml -f .\Olympiad-Back\docker-compose.production.yml -f .\Olympiad-Front\docker-compose.yml $args
+    docker-compose -f .\docker-compose.yml -f .\Olympiad-Back\docker-compose.yml -f .\Olympiad-Back\docker-compose.prod.yml -f .\Olympiad-Front\docker-compose.yml -f .\docker-compose.prod.yml $args
 }
 Set-Alias -Name odc OlympiadDockerCompose
 Set-Alias -Name odcs OlympiadDockerComposeStack
