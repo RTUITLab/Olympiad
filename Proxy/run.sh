000bb65369616e1a7c243a942cfdb7e09c85f5de
Maksim Makushchenko <maksalmak@gmail.com>
2021-03-16 18:24:03 +0300

    Add password generator for admin section and results viewer

--- Proxy/run.sh
+++ Proxy/run.sh
@@ -2,16 +2,30 @@
 
 set -e
 
-if [ -z $BASIC_AUTH_USERNAME ]; then
-  echo >&2 "BASIC_AUTH_USERNAME must be set"
+if [ -z $BASIC_AUTH_USERNAME_ADMIN ]; then
+  echo >&2 "BASIC_AUTH_USERNAME_ADMIN must be set"
   exit 1
 fi
 
-if [ -z $BASIC_AUTH_PASSWORD ]; then
-  echo >&2 "BASIC_AUTH_PASSWORD must be set"
+if [ -z $BASIC_AUTH_PASSWORD_ADMIN ]; then
+  echo >&2 "BASIC_AUTH_PASSWORD_ADMIN must be set"
   exit 1
 fi
 
-htpasswd -bBc /etc/nginx/.htpasswd $BASIC_AUTH_USERNAME $BASIC_AUTH_PASSWORD
+htpasswd -bBc /etc/nginx/admin.htpasswd $BASIC_AUTH_USERNAME_ADMIN $BASIC_AUTH_PASSWORD_ADMIN
+
+
+if [ -z $BASIC_AUTH_USERNAME_RESULTS_VIEWER ]; then
+  echo >&2 "BASIC_AUTH_USERNAME_ADMIN must be set"
+  exit 1
+fi
+
+if [ -z $BASIC_AUTH_PASSWORD_RESULTS_VIEWER ]; then
+  echo >&2 "BASIC_AUTH_PASSWORD_ADMIN must be set"
+  exit 1
+fi
+
+htpasswd -bBc /etc/nginx/results-viewer.htpasswd $BASIC_AUTH_USERNAME_RESULTS_VIEWER $BASIC_AUTH_PASSWORD_RESULTS_VIEWER
+
 
 exec nginx -g "daemon off;"
